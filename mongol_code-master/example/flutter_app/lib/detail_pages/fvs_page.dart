62a5ff4dd89ebdad76c25bbb299073750cc09d63
KeikaJames <gabira@bayagud.com>
2026-05-26 15:07:07 +0800

    Add mongol_code project

---
+++ mongol_code-master/example/flutter_app/lib/detail_pages/fvs_page.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:mongol_code/mongol_code.dart';
+
+class FvsPage extends StatefulWidget {
+  const FvsPage({super.key});
+
+  @override
+  State<FvsPage> createState() => _FvsPageState();
+}
+
+class _FvsPageState extends State<FvsPage> {
+  final fvs1 = String.fromCharCode(Mongol.fvs1);
+  final fvs2 = String.fromCharCode(Mongol.fvs2);
+  final fvs3 = String.fromCharCode(Mongol.fvs3);
+  final fvs4 = String.fromCharCode(Mongol.fvs4);
+  final nirugu = String.fromCharCode(Mongol.nirugu);
+
+  final List<String> mongolianCharacters = List.generate(
+    0x1842 - 0x1820 + 1,
+    (index) => String.fromCharCode(0x1820 + index),
+  );
+
+  Widget _buildVariantRow(
+    String label,
+    String text, {
+    double fontSize = 32,
+    double indent = 32,
+  }) {
+    return Row(
+      children: [
+        SizedBox(width: indent),
+        Text(label),
+        SizedBox(width: 16),
+        Text(text, style: TextStyle(fontSize: fontSize)),
+      ],
+    );
+  }
+
+  String _getVariant(String text) {
+    return convertUnicodeToMenksoft(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mongolian Characters')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: ListView.builder(
+          itemCount: mongolianCharacters.length,
+          itemBuilder: (context, index) {
+            final char = mongolianCharacters[index];
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'U+${(0x1820 + index).toRadixString(16).toUpperCase()}',
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(width: 16),
+                    Text(char, style: const TextStyle(fontSize: 44)),
+                  ],
+                ),
+                _buildVariantRow(
+                  'ISOLATE',
+                  _getVariant(char),
+                  fontSize: 44,
+                  indent: 16,
+                ),
+                _buildVariantRow('FVS1', _getVariant('$char$fvs1')),
+                _buildVariantRow('FVS2', _getVariant('$char$fvs2')),
+                _buildVariantRow('FVS3', _getVariant('$char$fvs3')),
+                _buildVariantRow('FVS4', _getVariant('$char$fvs4')),
+                _buildVariantRow(
+                  'INITIAL',
+                  _getVariant('$char$nirugu'),
+                  fontSize: 44,
+                  indent: 16,
+                ),
+                _buildVariantRow('FVS1', _getVariant('$char$fvs1$nirugu')),
+                _buildVariantRow('FVS2', _getVariant('$char$fvs2$nirugu')),
+                _buildVariantRow('FVS3', _getVariant('$char$fvs3$nirugu')),
+                _buildVariantRow('FVS4', _getVariant('$char$fvs4$nirugu')),
+                _buildVariantRow(
+                  'MEDIAL',
+                  _getVariant('$nirugu$char$nirugu'),
+                  fontSize: 44,
+                  indent: 16,
+                ),
+                _buildVariantRow(
+                  'FVS1',
+                  _getVariant('$nirugu$char$fvs1$nirugu'),
+                ),
+                _buildVariantRow(
+                  'FVS2',
+                  _getVariant('$nirugu$char$fvs2$nirugu'),
+                ),
+                _buildVariantRow(
+                  'FVS3',
+                  _getVariant('$nirugu$char$fvs3$nirugu'),
+                ),
+                _buildVariantRow(
+                  'FVS4',
+                  _getVariant('$nirugu$char$fvs4$nirugu'),
+                ),
+                _buildVariantRow(
+                  'FINAL',
+                  _getVariant('$nirugu$char'),
+                  fontSize: 44,
+                  indent: 16,
+                ),
+                _buildVariantRow('FVS1', _getVariant('$nirugu$char$fvs1')),
+                _buildVariantRow('FVS2', _getVariant('$nirugu$char$fvs2')),
+                _buildVariantRow('FVS3', _getVariant('$nirugu$char$fvs3')),
+                _buildVariantRow('FVS4', _getVariant('$nirugu$char$fvs4')),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
