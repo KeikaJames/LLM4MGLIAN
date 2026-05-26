679196a2e66d8e919c7c2ef5d53093f4d578efd0
KeikaJames <gabira@bayagud.com>
2026-05-26 22:04:26 +0800

    Add traditional Mongolian suffix inventory

---
+++ Tokenizer/traditional_mongolian/__init__.py
@@ -0,0 +1,21 @@
+from .core_traditional_mongolian_suffixes import (
+    ALL_SUFFIXES,
+    ALL_SUFFIXES_BY_ORDER,
+    CONTROL_CHARS,
+    CORE_TRADITIONAL_MONGOLIAN_SUFFIXES,
+    iter_surfaces,
+    strip_controls,
+    validate_suffix_inventory,
+    with_nnbsp,
+)
+
+__all__ = [
+    "ALL_SUFFIXES",
+    "ALL_SUFFIXES_BY_ORDER",
+    "CONTROL_CHARS",
+    "CORE_TRADITIONAL_MONGOLIAN_SUFFIXES",
+    "iter_surfaces",
+    "strip_controls",
+    "validate_suffix_inventory",
+    "with_nnbsp",
+]
